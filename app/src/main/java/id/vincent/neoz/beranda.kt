842c0b72e072306dee282b70871348f00e75a4e1
Mikaela <166426938+Mikaelazzz@users.noreply.github.com>
2024-12-10 16:45:41 +0700

    up

--- app/src/main/java/id/vincent/neoz/beranda.kt
+++ app/src/main/java/id/vincent/neoz/beranda.kt
@@ -1,8 +1,23 @@
 package id.vincent.neoz
 
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -12,7 +27,33 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import com.google.gson.annotations.SerializedName
 
+
+
+object NetworkUtil {
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isConnected(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
+
+class NetworkChangeReceiver(private val onNetworkChange: (Boolean) -> Unit) : BroadcastReceiver() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        context?.let {
+            val isConnected = NetworkUtil.isConnected(it)
+            onNetworkChange(isConnected)
+        }
+    }
+}
+
+
 class beranda : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var networkChangeReceiver: NetworkChangeReceiver
+    private var isWaitingForUserAction = false // Flag to control if user needs to press the button
 
     // Data class Hero
     data class Hero(
@@ -22,26 +63,32 @@ class beranda : AppCompatActivity(), NavigationView.OnNavigationItemSelectedList
         @SerializedName("role") val role: String
     )
 
+    private var currentFragment: Fragment? = null // Variabel untuk menyimpan fragment saat ini
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beranda)
 
+        if (savedInstanceState != null) {
+            currentFragment = supportFragmentManager.getFragment(savedInstanceState, "currentFragment")
+        }
+
+        setupNetworkReceiver()
+        checkInternetAndSetup()
+
         // Setup Toolbar
-//        val typeface = ResourcesCompat.getFont(this,R.font.poppinssemibold)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        val titletool: TextView = findViewById(R.id.toolbartitle)
+        val titleTool: TextView = findViewById(R.id.toolbartitle)
         setSupportActionBar(toolbar)
 
-        // Set the initial toolbar title to "Home"
-        titletool.text = "Home"
+        titleTool.text = "Home"
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         // Setup DrawerLayout
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
 
-        // Setup ActionBarDrawerToggle
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, toolbar,
             R.string.navigation_drawer_open,
@@ -51,12 +98,99 @@ class beranda : AppCompatActivity(), NavigationView.OnNavigationItemSelectedList
         toggle.syncState()
 
         navigationView.setNavigationItemSelectedListener(this)
-        replaceFragment(patch())
-
     }
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        currentFragment?.let {
+            supportFragmentManager.putFragment(outState, "currentFragment", it)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setupNetworkReceiver() {
+        try {
+            networkChangeReceiver = NetworkChangeReceiver { isConnected ->
+                if (isConnected) {
+                    if (isWaitingForUserAction) {
+                        showNoInternetLayout()
+                    } else {
+                        isWaitingForUserAction = true
+                        Toast.makeText(this, "Koneksi internet kembali!", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    showNoInternetLayout()
+                }
+            }
+
+            val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+            registerReceiver(networkChangeReceiver, filter)
+        } catch (e: Exception) {
+            e.printStackTrace() // Handle exceptions
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkInternetAndSetup() {
+        if (NetworkUtil.isConnected(this)) {
+            setupMainLayout()
+        } else {
+            showNoInternetLayout()
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setupMainLayout() {
+        if (isNetworkConnected()) {
+            // Jika koneksi internet kembali, tampilkan fragment terakhir yang disimpan
+            if (currentFragment != null) {
+                replaceFragment(currentFragment!!)
+            } else {
+                // Jika tidak ada fragment yang tersimpan, tampilkan fragment default (misalnya, patch())
+                replaceFragment(patch())
+            }
+        } else {
+            showNoInternetLayout()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showNoInternetLayout() {
+        // Avoid switching layout too often
+        setContentView(R.layout.internet)
+
+        val retryButton: Button = findViewById(R.id.button_signal)
+        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
+
+        retryButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            retryButton.isEnabled = false
+
+            // Retry network check with delay
+            Handler(Looper.getMainLooper()).postDelayed({
+                if (NetworkUtil.isConnected(this)) {
+                    setupMainLayout() // Reload main layout
+                    isWaitingForUserAction = false
+                } else {
+                    Toast.makeText(this, "Tidak ada koneksi internet!", Toast.LENGTH_SHORT).show()
+                    progressBar.visibility = View.GONE
+                    retryButton.isEnabled = true
+                }
+            }, 2500) // Delay for checking again
+        }
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetworkInfo
+        return activeNetwork?.isConnected == true
+    }
+
+
     private fun replaceFragment(fragment: Fragment) {
-        // Hide the ScrollView content
+        currentFragment = fragment
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .commit()
@@ -68,6 +202,15 @@ class beranda : AppCompatActivity(), NavigationView.OnNavigationItemSelectedList
             R.id.home -> {
                 titletool.text = "Home"
                 replaceFragment(patch())
+            }
+            R.id.game -> {
+                titletool.text = "Find Nick Game"
+                replaceFragment(findergame())
+            }
+            R.id.filter -> {
+                titletool.text = "Filter Draft"
+                replaceFragment(draft())
+
             }
             R.id.hero -> {
                 titletool.text = "Hero"
@@ -124,5 +267,9 @@ class beranda : AppCompatActivity(), NavigationView.OnNavigationItemSelectedList
         } else {
             super.onBackPressed()
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(networkChangeReceiver)
     }
 }
--- app/src/main/java/id/vincent/neoz/draft.kt
+++ app/src/main/java/id/vincent/neoz/draft.kt
@@ -31,26 +31,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
 
-object NetworkUtil {
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun isConnected(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork = connectivityManager.activeNetwork ?: return false
-        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
-        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-    }
-}
-
-class NetworkChangeReceiver(private val onNetworkChange: (Boolean) -> Unit) : BroadcastReceiver() {
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onReceive(context: Context?, intent: Intent?) {
-        context?.let {
-            val isConnected = NetworkUtil.isConnected(it)
-            onNetworkChange(isConnected)
-        }
-    }
-}
-
 class HeroShuffleManager(
     private val mainActivity: FragmentActivity,
     private val heroData: List<HeroItem>,
@@ -124,7 +104,6 @@ class HeroShuffleManager(
             }
         }
 
-
         override fun getItemCount() = heroes.size
     }
 
@@ -152,21 +131,17 @@ class HeroShuffleManager(
         shuffleButton.setOnClickListener {
             val selectedHeroes: List<HeroShuffleManager.HeroItem> = when {
                 !funCheckbox.isChecked && !roleCheckbox.isChecked -> {
-
-//                    mainActivity.showToast("Please select a mode")
-//                    Toast.makeText(requireContext(), "Silakan pilih mode terlebih dahulu!", Toast.LENGTH_SHORT).show()
                     return@setOnClickListener // Menghentikan eksekusi jika tidak ada mode yang dipilih
                 }
                 !player3Checkbox.isChecked && !player5Checkbox.isChecked -> {
-//                    Toast.makeText(requireContext(), "Silakan pilih jumlah pemain", Toast.LENGTH_SHORT).show()
                     return@setOnClickListener
                 }
                 funCheckbox.isChecked -> {
-                    shuffleFunMode() // Fungsi ini tetap dipanggil, tetapi hasilnya diabaikan
-                    listOf() // Inisialisasi list kosong (karena fungsi tidak mengembalikan nilai)
+                    shuffleFunMode()
+                    listOf()
                 }
                 roleCheckbox.isChecked -> {
-                    shuffleHeroesByRole() // Fungsi ini tetap dipanggil, tetapi hasilnya diabaikan
+                    shuffleHeroesByRole()
                     listOf()
                 }
                 else -> {
@@ -185,125 +160,74 @@ class HeroShuffleManager(
             hideTextView.visibility = View.GONE
             heroesRecyclerView.visibility = View.VISIBLE
         }
-
     }
 
     private fun updateRecyclerViewSequentially(heroes: List<HeroItem>) {
-        val availableHeroes = heroes.toMutableList() // Copy of heroes to prevent modifying original list
-        val selectedHeroes = mutableListOf<HeroItem>() // To store selected heroes
+        val availableHeroes = heroes.toMutableList()
+        val selectedHeroes = mutableListOf<HeroItem>()
 
-        // Coroutine untuk menambahkan efek visual "gacha"
         CoroutineScope(Dispatchers.Main).launch {
-            // Set RecyclerView untuk efek gacha
-            // Jangan menggunakan GridLayoutManager dengan ukuran hero yang dinamis, gunakan LinearLayoutManager jika perlu.
             heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false)
 
             heroesRecyclerView.adapter = HeroAdapter(listOf()) // Start with an empty adapter
 
-            // Rapidly shuffle dan update RecyclerView untuk efek visual
-            for (i in 1..20) { // Show swapping effect for 20 cycles
-                delay(500) // Short delay to simulate fast swapping
-                val shuffledHeroes = availableHeroes.shuffled() // Shuffle heroes for each cycle
-                heroesRecyclerView.adapter = HeroAdapter(shuffledHeroes) // Update dengan shuffled heroes
+            for (i in 1..20) {
+                delay(500)
+                val shuffledHeroes = availableHeroes.shuffled()
+                heroesRecyclerView.adapter = HeroAdapter(shuffledHeroes)
             }
 
-            // Ambil heroes berdasarkan jumlah pemain yang dipilih
             val playerCount = if (player3Checkbox.isChecked) 3 else 5
 
-            // Loop untuk memilih hero untuk setiap player
             for (i in 1..playerCount) {
-                delay(0) // Small delay before picking the next hero
-                val heroToPick = availableHeroes.random() // Pilih hero acak dari yang tersedia
-                selectedHeroes.add(heroToPick) // Tambahkan hero yang dipilih ke list
-                availableHeroes.remove(heroToPick) // Hapus hero yang dipilih untuk mencegah seleksi ulang
+                delay(0)
+                val heroToPick = availableHeroes.random()
+                selectedHeroes.add(heroToPick)
+                availableHeroes.remove(heroToPick)
             }
 
-            // Tampilkan heroes yang dipilih setelah shuffle
-            heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false) // Pastikan horizontal
-
-            // Update adapter hanya sekali setelah shuffle selesai
-            heroesRecyclerView.adapter = HeroAdapter(selectedHeroes) // Update adapter untuk menampilkan heroes yang dipilih
+            heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false)
+            heroesRecyclerView.adapter = HeroAdapter(selectedHeroes)
         }
     }
-
-
-
 
     private fun shuffleFunMode() {
         val hideTextView = mainActivity.findViewById<TextView>(R.id.hidetext)
 
         CoroutineScope(Dispatchers.Main).launch {
-            // Copy hero data untuk diacak
             val availableHeroes = heroData.toMutableList()
             val selectedHeroes = mutableListOf<HeroItem>()
-
-            // Batasi jumlah pemain berdasarkan checkbox
             val playerCount = if (player3Checkbox.isChecked) 3 else 5
 
-            // RecyclerView sementara untuk efek gacha
             heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false)
-            heroesRecyclerView.adapter = HeroAdapter(listOf()) // Kosongkan awalnya
+            heroesRecyclerView.adapter = HeroAdapter(listOf())
 
-            // Lakukan swapping visual untuk efek gacha
             for (cycle in 1..20) {
-                delay(50) // Durasi swapping per siklus (bisa diatur)
-                val tempHeroes = availableHeroes.shuffled().take(playerCount) // Ambil sejumlah pemain
+                delay(50)
+                val tempHeroes = availableHeroes.shuffled().take(playerCount)
                 val tempAdapterHeroes = tempHeroes.map {
-                    // Tampilkan hero acak dengan role acak sementara
                     it.copy(role = listOf("Mage", "Gold", "Jung", "Roam", "Exp").random())
                 }
-                heroesRecyclerView.adapter = HeroAdapter(tempAdapterHeroes) // Tampilkan acakan sementara
+                heroesRecyclerView.adapter = HeroAdapter(tempAdapterHeroes)
             }
 
-            // Setelah swapping selesai, tentukan hasil akhir
             while (selectedHeroes.size < playerCount) {
                 val heroToPick = availableHeroes.random()
                 val randomRole = listOf("Mage", "Gold", "Jung", "Roam", "Exp").random()
 
-                // Pastikan hero atau role tidak terduplikasi
                 if (selectedHeroes.any { it.hero == heroToPick.hero || it.role == randomRole }) continue
 
-                // Tambahkan hero ke daftar hasil akhir
                 selectedHeroes.add(heroToPick.copy(role = randomRole))
                 availableHeroes.remove(heroToPick)
             }
 
-            // Tampilkan hasil akhir pada RecyclerView
             heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false)
-            heroesRecyclerView.adapter = HeroAdapter(selectedHeroes) // Tampilkan hasil akhir
+            heroesRecyclerView.adapter = HeroAdapter(selectedHeroes)
 
             hideTextView.visibility = View.GONE
             heroesRecyclerView.visibility = View.VISIBLE
-
-            // Tampilkan pesan pop-up setelah selesai swapping
-//            showFunModeMessage()
         }
     }
-
-//    // Fungsi untuk menampilkan AlertDialog setelah selesai swapping
-//    private fun showFunModeMessage() {
-//        val builder = android.app.AlertDialog.Builder(mainActivity)
-//
-//        // Set message and button
-//        builder.setMessage("Gunakan hero ini hanya di mode Clasic yaa..")
-//            .setCancelable(false)
-//            .setPositiveButton("OK") { dialog, id ->
-//                // Menutup dialog ketika tombol OK ditekan
-//                dialog.dismiss()
-//            }
-//
-//        // Menambahkan custom layout untuk AlertDialog
-//        val alert = builder.create()
-//
-//        // Menetapkan background dari drawable
-//        alert.window?.setBackgroundDrawableResource(R.drawable.back)
-//
-//        // Menampilkan dialog
-//        alert.show()
-//    }
-
-
-
 
     private fun shuffleHeroesByRole() {
         val hideTextView = mainActivity.findViewById<TextView>(R.id.hidetext)
@@ -312,55 +236,42 @@ class HeroShuffleManager(
             val availableHeroes = heroData.toMutableList()
             val selectedHeroes = mutableListOf<HeroItem>()
 
-            // Cek apakah heroData tidak kosong
             if (availableHeroes.isEmpty()) {
                 return@launch
             }
 
             val playerCount = if (player3Checkbox.isChecked) 3 else 5
 
-            // RecyclerView sementara untuk efek gacha
             heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false)
-            heroesRecyclerView.adapter = HeroAdapter(listOf()) // Kosongkan awalnya
+            heroesRecyclerView.adapter = HeroAdapter(listOf())
 
-
-            // Tampilkan efek shuffle visual selama beberapa iterasi
             for (cycle in 1..20) {
-                delay(50) // Memberikan sedikit jeda di antara setiap swap untuk efek yang lebih smooth
-
-                // Shuffle heroes secara acak untuk efek visual
+                delay(50)
                 val tempHeroes = availableHeroes.shuffled().take(playerCount)
-                val tempAdapterHeroes = tempHeroes.map { it.copy() }  // Tidak ubah role untuk visual effect
+                val tempAdapterHeroes = tempHeroes.map { it.copy() }
 
-                // Update RecyclerView dengan hero yang di-shuffle
                 heroesRecyclerView.adapter = HeroAdapter(tempAdapterHeroes)
             }
 
-            // Pilih hero yang unik berdasarkan role setelah proses shuffle selesai
             while (selectedHeroes.size < playerCount) {
                 val heroToPick = availableHeroes.random()
-                val randomRole = heroToPick.role // Dapatkan role dari hero yang dipilih
+                val randomRole = heroToPick.role
 
-                // Pastikan hero atau role tidak terduplikasi
                 if (selectedHeroes.any { it.hero == heroToPick.hero || it.role == randomRole }) {
-                    continue // Jika hero atau role sudah ada, lanjutkan ke hero berikutnya
+                    continue
                 }
 
-                // Jika tidak ada duplikat, tambahkan hero yang dipilih
                 selectedHeroes.add(heroToPick)
                 availableHeroes.remove(heroToPick)
             }
 
-            // Update RecyclerView dengan hero yang dipilih secara unik
             heroesRecyclerView.layoutManager = LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false)
-            heroesRecyclerView.adapter = HeroAdapter(selectedHeroes)  // Update adapter dengan heroes yang dipilih
+            heroesRecyclerView.adapter = HeroAdapter(selectedHeroes)
 
-            // Tampilkan hasil akhir pada RecyclerView
             hideTextView.visibility = View.GONE
             heroesRecyclerView.visibility = View.VISIBLE
         }
     }
-
 
     private fun updateRecyclerView(heroes: List<HeroItem>) {
         heroesRecyclerView.layoutManager = GridLayoutManager(mainActivity, heroes.size)
@@ -383,107 +294,27 @@ class HeroShuffleManager(
     }
 }
 
-// Extension function to show toast in MainActivity
-fun MainActivity.showToast(message: String) {
-    android.widget.Toast.makeText(this, message, android.widget.Toast.LENGTH_SHORT).show()
-}
-
 class draft : Fragment() {
     private lateinit var heroShuffleManager: HeroShuffleManager
-    private lateinit var networkChangeReceiver: NetworkChangeReceiver
-    private var isWaitingForUserAction = false // Flag to control if user needs to press the button
 
-    @RequiresApi(Build.VERSION_CODES.M)
-    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.draft, container, false)
 
-        // Register NetworkChangeReceiver
-        try {
-            networkChangeReceiver = NetworkChangeReceiver { isConnected ->
-                if (isConnected) {
-                    // Jika terhubung, tampilkan layout 'Coba Lagi' dan tunggu aksi pengguna
-                    if (isWaitingForUserAction) {
-                        showNoInternetLayout(view) // Tampilkan layout no internet hingga user menekan tombol
-                    } else {
-                        // Koneksi internet pulih, tunggu pengguna menekan "Coba Lagi"
-                        isWaitingForUserAction = true
-                        Toast.makeText(requireContext(), "Koneksi internet kembali!", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    // Tidak ada koneksi internet, tampilkan layout no internet
-                    showNoInternetLayout(view)
-                }
-            }
-
-            val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-            requireActivity().registerReceiver(networkChangeReceiver, filter)
-
-            // Inisialisasi layout sesuai kondisi awal
-            checkInternetAndSetup(view)
-        } catch (e: Exception) {
-            e.printStackTrace() // Menangani exception
-        }
+        setupMainLayout(view)
 
         return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        try {
-            requireActivity().unregisterReceiver(networkChangeReceiver)
-        } catch (e: IllegalArgumentException) {
-            // Abaikan jika receiver belum terdaftar
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun checkInternetAndSetup(view: View) {
-        if (NetworkUtil.isConnected(requireContext())) {
-            setupMainLayout(view)
-        } else {
-            showNoInternetLayout(view)
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun showNoInternetLayout(view: View) {
-        val retryButton: Button = view.findViewById(R.id.button_signal)
-        val progressBar: ProgressBar = view.findViewById(R.id.progress_bar)
-
-        // Tombol untuk mencoba ulang
-        retryButton.setOnClickListener {
-            // Menampilkan ProgressBar
-            progressBar.visibility = View.VISIBLE
-            retryButton.isEnabled = false  // Menonaktifkan tombol agar tidak bisa ditekan selama pengecekan
-
-            // Lakukan pengecekan koneksi dengan delay
-            Handler(Looper.getMainLooper()).postDelayed({
-                if (NetworkUtil.isConnected(requireContext())) {
-                    setupMainLayout(view) // Muat ulang layout utama jika internet tersedia
-                    isWaitingForUserAction = false // Reset waiting flag
-                } else {
-                    Toast.makeText(requireContext(), "Tidak ada koneksi internet!", Toast.LENGTH_SHORT).show()
-                    progressBar.visibility = View.GONE  // Menyembunyikan progress bar
-                    retryButton.isEnabled = true  // Mengaktifkan kembali tombol "Coba Lagi"
-                }
-            }, 2500) // Delay selama 2.5 detik
-        }
-    }
-
     private fun setupMainLayout(view: View) {
-        // Mengatur layout setelah koneksi internet berhasil
         val context = requireContext()
         val layoutInflater = LayoutInflater.from(context)
         layoutInflater.inflate(R.layout.draft, null)
 
-        // Membaca data JSON hero dari file assets
         val jsonString = context.assets.open("heroesdraft.json").bufferedReader().use { it.readText() }
 
-        // Inisialisasi HeroShuffleManager
         heroShuffleManager = HeroShuffleManager(
             mainActivity = requireActivity(),
             heroData = HeroShuffleManager.parseHeroData(jsonString),
@@ -495,7 +326,6 @@ class draft : Fragment() {
             shuffleButton = view.findViewById(R.id.shuffle)
         )
 
-        // Setup logika shuffle
         heroShuffleManager.setupShuffleLogic()
     }
 }
